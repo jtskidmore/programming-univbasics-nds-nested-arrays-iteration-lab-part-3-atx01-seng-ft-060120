600fa8a5ee9b6879e24940204ed8c195ae25328f
JT Skidmore <johntoddskidmore@gmail.com>
2020-05-02 20:46:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,6 @@
 def join_nested_strings(src)
   count = 0 
-  sum = "" 
+  sum = []
   while count < src.count do
     
     inner_count = 0 
@@ -22,6 +22,6 @@ def join_nested_strings(src)
   end
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-  sum
+  sum.join(" ")
   
 end
